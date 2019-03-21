bea700abd468429f4e82cdf84c5114f2e376da53
Henrik <henrik.hirvonen77@gmail.com>
2019-03-21 17:15:21 +0200

    osio 2 tehty

--- laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -3,6 +3,13 @@ package com.mycompany.unicafe;
 public class Paaohjelma {
 
     public static void main(String[] args) {
+        
+        Maksukortti testiKortti = new Maksukortti(1000);
+        System.out.println(testiKortti.toString());
+        
+        
+        
+        
         Kassapaate unicafeExactum = new Kassapaate();
         Maksukortti kortti = new Maksukortti(10000);
         
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,50 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void kortinLataaminenToimii() {
+        kortti.lataaRahaa(110);
+        assertEquals("saldo: 11.10", kortti.toString());
+    }
+
+    @Test
+    public void negatiivinenLatausEiTeeMitaan() {
+        kortti.lataaRahaa(-1100);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void kortinRahanNostaminenOnnistuuMaara() {
+        kortti.otaRahaa(450);
+        assertEquals("saldo: 5.50", kortti.toString());
+    }
+
+    @Test
+    public void kortinRahanNostaminenOnnistuuBoolean() {
+        assertTrue(kortti.otaRahaa(450));
+    }
+
+    @Test
+    public void kortiltaEiVoiNostaaLiikaaMaara() {
+        kortti.otaRahaa(1500);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void kortiltaEiVoiNostaaLiikaaBoolean() {
+        assertFalse(kortti.otaRahaa(1500));
     }
 }
