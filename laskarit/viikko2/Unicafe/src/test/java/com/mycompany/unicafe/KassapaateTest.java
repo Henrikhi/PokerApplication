f3b62515b22eed0932ccec0621efc60e10e4abda
Henrik <henrik.hirvonen77@gmail.com>
2019-03-22 12:09:32 +0200

    unicafeHoidettu

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -9,45 +9,168 @@ import static org.junit.Assert.*;
 
 public class KassapaateTest {
 
-    public KassapaateTest() {
-    }
+    Kassapaate kassapaate;
+    Maksukortti maksukortti;
 
     @Before
     public void setUp() {
+        this.kassapaate = new Kassapaate();
+        this.maksukortti = new Maksukortti(1000);
     }
 
-    /*
-    Tee testiluokka KassapaateTest ja tee testit jotka testaavat ainakin seuraavia asioita:
+    @Test
+    public void alustusRahaOK() {
+        assertEquals(kassapaate.kassassaRahaa(), 100000);
+    }
 
-luodun kassapäätteen rahamäärä ja myytyjen 
-    lounaiden määrä on oikea (rahaa 1000, lounaita myyty 0)
-    
-käteisosto toimii sekä edullisten että maukkaiden lounaiden osalta:
-    
--jos maksu riittävä: kassassa oleva rahamäärä kasvaa lounaan
-    hinnalla ja vaihtorahan suuruus on oikea
--jos maksu on riittävä: myytyjen lounaiden määrä kasvaa
--jos maksu ei ole riittävä: kassassa oleva rahamäärä ei muutu,
-    kaikki rahat palautetaan vaihtorahana ja myytyjen lounaiden 
-    määrässä ei muutosta
+    @Test
+    public void alustusEdullisiaMyyty0() {
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 0);
+    }
 
-    seuraavissa testeissä tarvitaan myös Maksukorttia jonka oletetaan 
-    toimivan oikein
-korttiosto toimii sekä edullisten että maukkaiden lounaiden osalta:
--jos kortilla on tarpeeksi rahaa, veloitetaan summa kortilta ja
-    palautetaan true
--jos kortilla on tarpeeksi rahaa, myytyjen lounaiden määrä kasvaa
--jos kortilla ei ole tarpeeksi rahaa, kortin rahamäärä ei muutu, 
-    myytyjen lounaiden määrä muuttumaton ja palautetaan false
--kassassa oleva rahamäärä ei muutu kortilla ostettaessa
+    @Test
+    public void alustusMaukkaitaMyyty0() {
+        assertEquals(kassapaate.maukkaitaLounaitaMyyty(), 0);
+    }
+
+    @Test
+    public void syoEdullisestiKorttiBooleanTrue() {
+        assertTrue(kassapaate.syoEdullisesti(maksukortti));
+    }
+
+    @Test
+    public void syoEdullisestiKorttiBooleanFalse() {
+        Maksukortti kortti = new Maksukortti(10);
+        assertFalse(kassapaate.syoEdullisesti(kortti));
+    }
+
+    @Test
+    public void syoMaukkaastiKorttiBooleanTrue() {
+        assertTrue(kassapaate.syoMaukkaasti(maksukortti));
+    }
+
+    @Test
+    public void syoMaukkaastiKorttiBooleanFalse() {
+        Maksukortti kortti = new Maksukortti(10);
+        assertFalse(kassapaate.syoMaukkaasti(kortti));
+    }
+
+    @Test
+    public void syoEdullisestiKorttiMaaraMuuttuu() {
+        kassapaate.syoEdullisesti(maksukortti);
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 1);
+    }
+
+    @Test
+    public void syoEdullisestiKorttiMaaraEiMuutu() {
+        Maksukortti kortti = new Maksukortti(10);
+        kassapaate.syoEdullisesti(kortti);
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 0);
+    }
+
+    @Test
+    public void syoMaukkaastiKorttiMaaraMuuttuu() {
+        kassapaate.syoMaukkaasti(maksukortti);
+        assertEquals(kassapaate.maukkaitaLounaitaMyyty(), 1);
+    }
+
+    @Test
+    public void syoMaukkaastiKorttiMaaraEiMuutu() {
+        Maksukortti kortti = new Maksukortti(10);
+        kassapaate.syoMaukkaasti(kortti);
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 0);
+    }
+
+    @Test
+    public void syoEdullisestiKassaTasaraha() {
+        assertEquals(kassapaate.syoEdullisesti(240), 0);
+    }
+
+    @Test
+    public void syoEdullisestiKassaVaihtorahaOK() {
+        assertEquals(kassapaate.syoEdullisesti(260), 20);
+    }
+
+    @Test
+    public void syoEdullisestiKassaLiianVahanRahaa() {
+        assertEquals(kassapaate.syoEdullisesti(200), 200);
+    }
+
+    @Test
+    public void syoEdullisestiKassaMaaraKasvaa() {
+        kassapaate.syoEdullisesti(500);
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 1);
+    }
+
+    @Test
+    public void syoEdullisestiKassaMaara0() {
+        kassapaate.syoEdullisesti(200);
+        assertEquals(kassapaate.edullisiaLounaitaMyyty(), 0);
+    }
+
+    @Test
+    public void syoMaukkaastiKassaTasaraha() {
+        assertEquals(kassapaate.syoMaukkaasti(400), 0);
+    }
+
+    @Test
+    public void syoMaukkaastiKassaVaihtorahaOK() {
+        assertEquals(kassapaate.syoMaukkaasti(4500), 4100);
+    }
+
+    @Test
+    public void syoMaukkaastiKassaLiianVahanRahaa() {
+        assertEquals(kassapaate.syoMaukkaasti(300), 300);
+    }
+
+    @Test
+    public void syoMaukkaastiKassaMaaraKasvaa() {
+        kassapaate.syoMaukkaasti(500);
+        assertEquals(kassapaate.maukkaitaLounaitaMyyty(), 1);
+    }
+
+    @Test
+    public void syoMaukkaastiKassaMaara0() {
+        kassapaate.syoEdullisesti(399);
+        assertEquals(kassapaate.maukkaitaLounaitaMyyty(), 0);
+    }
+
+    @Test
+    public void kassaanEiVaikutaKorttimaksuEdullinen() {
+        int kassaAlku = kassapaate.kassassaRahaa();
+        kassapaate.syoEdullisesti(maksukortti);
+        assertEquals(kassaAlku, kassapaate.kassassaRahaa());
+    }
+
+    @Test
+    public void kassaanEiVaikutaKorttimaksuMaukas() {
+        int kassaAlku = kassapaate.kassassaRahaa();
+        kassapaate.syoMaukkaasti(maksukortti);
+        assertEquals(kassaAlku, kassapaate.kassassaRahaa());
+    }
+
+    @Test
+    public void lataaRahaaOnnistuuKortti() {
+        kassapaate.lataaRahaaKortille(maksukortti, 100);
+        assertEquals(maksukortti.saldo(), 1100);
+    }
+
+    @Test
+    public void lataaRahaaOnnistuuKassa() {
+        kassapaate.lataaRahaaKortille(maksukortti, 100);
+        assertEquals(kassapaate.kassassaRahaa(), 100100);
+    }
     
-kortille rahaa ladattaessa kortin saldo muuttuu ja kassassa oleva
-    rahamäärä kasvaa ladatulla summalla
+    @Test
+    public void lataaRahaaEiOnnistuKortti() {
+        kassapaate.lataaRahaaKortille(maksukortti, -100);
+        assertEquals(maksukortti.saldo(), 1000);
+    }
     
-Huomaat että kassapääte sisältää melkoisen määrän "copypastea".
-    Nyt kun kassapäätteellä on automaattiset testit, on sen 
-    rakennetta helppo muokata eli refaktoroida siistimmäksi 
-    koko ajan kuitenkin varmistaen, että testit menevät läpi. 
-    Tulemme tekemään refaktoroinnin myöhemmin kurssilla.
-     */
+    @Test
+    public void lataaRahaaEiOnnistuKassa() {
+        kassapaate.lataaRahaaKortille(maksukortti, -100);
+        assertEquals(kassapaate.kassassaRahaa(), 100000);
+    }
+
 }
